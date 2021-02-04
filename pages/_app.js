5c6080fcca9333e2085cd08de95f84e616618cd8
Adnan Ahmed <adnan.ahmed01@gmail.com>
2021-02-04 17:42:07 +0000

    added local link

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: 'api/graphql',
 });
 
 let token, guestId;
