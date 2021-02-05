8f305388491b76ddf739ca581102ab0b3185f983
Adnan Ahmed <adnan.ahmed01@gmail.com>
2021-02-05 00:18:51 +0000

    modified index page

--- pages/index.js
+++ pages/index.js
@@ -35,9 +35,11 @@ const Home = (props) => {
         <h3 className="text-2xl mb-8 text-gray-200">
           Divine products
         </h3>
-        <button className="bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider">
-          Pre Order
-        </button>
+        <Link href="/store">
+          <button className="bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider">
+            Pre Order
+          </button>
+        </Link>
       </div>
     </div>
     <section className="container mx-auto px-6 p-10">
@@ -47,7 +49,9 @@ const Home = (props) => {
       <div className="flex items-center flex-wrap mb-20">
         <div className="w-full md:w-1/2">
           <h4 className="text-3xl text-white font-bold mb-3">Fashion</h4>
-          <p className="text-white mb-8">Embrace the power of the ancient orysha</p>
+          <p className="text-white mb-8">Embrace the power of the ancient Orysha with our range of themed clothing. 
+   
+          </p>
         </div>
         <div className="w-full md:w-1/2">
          <img className="h-64 w-full object-contain" alt="african-angel" src="/rsz_orysha_winged.jpg"></img>
@@ -58,42 +62,43 @@ const Home = (props) => {
          <img className="h-64 w-full object-contain" alt="african-mask" src="/orysha_mask.jpg"></img>
         </div>
         <div className="w-full md:w-1/2 pl-10">
-          <h4 className="text-3xl text-white font-bold mb-3">Style</h4>
-          <p className="text-white mb-8">Our range of snapback caps offer an unprecedented range of styles and fashion.</p>
+          <h4 className="text-3xl text-white font-bold mb-3">Unique</h4>
+          <p className="text-white mb-8">
+          Each Orysha represents a specific quality - strength, kindness, feminity, power. Which Orysha matches your personality?</p>
         </div>
       </div>
       <div className="flex items-center flex-wrap mb-20">
         <div className="w-full md:w-1/2">
           <h4 className="text-3xl text-white font-bold mb-3">Taste</h4>
-          <p className="text-white mb-8">The orysha are an ancient group of gods</p>
+          <p className="text-white mb-8">Explore our range of custom jackets, tracksuits and snapback hats now.</p>
         </div>
         <div className="w-full md:w-1/2">
          <img className="h-64 w-full object-contain" alt="african-mask" src="/rsz_orysha_woman_mask.jpg"></img>
         </div>
       </div>
     </section>
-    <section className="container mx-auto bg-black">
+    <section className="container mx-auto bg-black border-solid border-4 border-orange-600">
       <div className="container mx-auto px-6 py-20">
-        <h2 className="text-4xl font-bold text-center text-orange-600 mb-8">
+        <h2 className="text-4xl font-bold text-center text-white mb-8">
           Testimonials
         </h2>
         <div className="flex flex-wrap">
           <div className="w-full md:w-1/3 px-2 mb-4">
-            <div className="bg-white rounded shadow py-2">
-              <p className="text-gray-800 text-base px-6 mb-5">Monitoring and tracking my health vitals anywhere I go and on any platform I use has never been easier.</p>
-              <p className="text-gray-500 text-xs md:text-sm px-6">John Doe</p>
+            <div className="bg-black rounded shadow py-2 border-solid border-2 border-white">
+              <p className="text-white text-base px-6 mb-5">The perfect cap - a dream match for my style. </p>
+              <p className="text-white text-xs md:text-sm px-6">John Doe</p>
             </div>
           </div>
           <div className="w-full md:w-1/3 px-2 mb-4">
-            <div className="bg-white rounded shadow py-2">
-              <p className="text-gray-800 text-base px-6 mb-5">As an Athlete, this is the perfect product for me. I wear my Smart Health Monitoring Wristwatch everywhere I go, even in the bathroom since it's waterproof.</p>
-              <p className="text-gray-500 text-xs md:text-sm px-6">Jane Doe</p>
+            <div className="bg-black rounded shadow py-2 border-solid border-2 border-orange-600">
+              <p className="text-white text-base px-6 mb-5">The handbags are classy and unique, all of my friends ask where I got them.</p>
+              <p className="text-white text-xs md:text-sm px-6">Jane Doe</p>
             </div>
           </div>
           <div className="w-full md:w-1/3 px-2 mb-4">
-            <div className="bg-white rounded shadow py-2">
-              <p className="text-gray-800 text-base px-6 mb-5">I don't regret buying this wearble gadget. One of the best gadgets I own!.</p>
-              <p className="text-gray-500 text-xs md:text-sm px-6">James Doe</p>
+            <div className="bg-black rounded shadow py-2 border-solid border-2 border-white">
+              <p className="text-white text-base px-6 mb-5">Totally unique</p>
+              <p className="text-white text-xs md:text-sm px-6">James Doe</p>
             </div>
           </div>
         </div>
@@ -101,7 +106,7 @@ const Home = (props) => {
   </section>
   <section className="bg-black container mx-auto" >
     <div className="container mx-auto px-6 text-center py-20">
-      <h2 className="mb-6 text-4xl font-bold text-center text-white">
+      <h2 className="mb-6 text-4xl font-bold text-center text-orange-600">
         Limited in Stock
       </h2>
       <h3 className="my-4 text-2xl text-white">
