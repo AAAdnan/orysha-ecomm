102462fdf1e67c073ab645e7aee8a158f9b26c06
Adnan Ahmed <adnan.ahmed01@gmail.com>
2021-02-19 00:29:57 +0000

    corrected css on product item page

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -8,17 +8,17 @@ const ProductItem = props => {
     return (  
       <>
       <div className="py-6">
-        <div className="flex max-w-2xl h-64 border-solid border-4 border-gray-600 bg-white rounded overflow-hidden shadow-lg">
-          <div className="w-1/2 p-8">
-            <img className="w-full object-cover" src={props.image}></img>
+        <div className="flex flex-col md:flex-row md:max-w-2xl h-64 border-solid border-4 border-gray-600 bg-white rounded overflow-hidden shadow-lg">
+          <div className="md:w-1/2 p-6 md:p-16">
+            <img src={props.image}></img>
           </div>
-          <div className="w-1/2 p-8">
+          <div className="flex flex-col items-center md:items-start md:self-center md:w-1/2 ">
                 <h1 className="text-black font-mono font-bold text-2xl">{ props.name }</h1>
-              <h1 className="text-black text-base">£{ props.price }</h1>
+                <h1 className="text-black text-base mb-2">£{ props.price }</h1>
               <Link href={`store/product/${props.id}`}>
                 <a>
-                    <button className="bg-black hover:bg-orange-600 rounded py-2 px-2 mt-6">
-                      <p className="text-white text-sm pr-8">VIEW</p>
+                    <button className="bg-black hover:bg-orange-600 rounded py-2 px-2 md:mt-6">
+                      <p className="text-white text-sm px-8">VIEW</p>
                     </button>
                 </a>
               </Link>
