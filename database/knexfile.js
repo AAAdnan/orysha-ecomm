8b6367daada232ca9bb26a50d8ea898b7706650e
Adnan Ahmed <adnan.ahmed01@gmail.com>
2021-09-06 00:58:14 +0100

    deleted yarn file

--- database/knexfile.js
+++ database/knexfile.js
@@ -1,17 +1,22 @@
 const path = require('path')
 require('dotenv').config()
 
-console.log(process.env.PGHOST)
+console.log(process.env.DATABASE_URL)
+
 
 module.exports = {
   client: 'pg',
-  connection: {
-    host: process.env.PGHOST,
-    user: process.env.PGUSER || 'postgres',
-    port: process.env.PGPORT || '5433',
-    password: process.env.PGPASSWORD || 'yasmeen60',
-    database: process.env.PGDATABASE || 'productdb',
-  },
+  connection:
+    // host: process.env.PGHOST,
+    // user: process.env.PGUSER || 'postgres',
+    // port: process.env.PGPORT || '5433',
+    // password: process.env.PGPASSWORD || 'yasmeen60',
+    // database: process.env.PGDATABASE || 'productdb',
+    process.env.DATABASE_URL,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    },
   migrations: {
     directory: path.join(__dirname, 'src', 'db', 'migrations')
   },
