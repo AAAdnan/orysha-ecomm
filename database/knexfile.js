37b0023b6e44129e4dc647c51afd2d152ea221ad
Adnan Ahmed <adnan.ahmed01@gmail.com>
2021-09-21 18:13:59 +0100

    knexfile changed

--- database/knexfile.js
+++ database/knexfile.js
@@ -6,17 +6,17 @@ console.log(process.env.DATABASE_URL)
 
 module.exports = {
   client: 'pg',
-  connection:
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  },
     // host: process.env.PGHOST,
     // user: process.env.PGUSER || 'postgres',
     // port: process.env.PGPORT || '5433',
     // password: process.env.PGPASSWORD || 'yasmeen60',
     // database: process.env.PGDATABASE || 'productdb',
-    process.env.DATABASE_URL,
-    // ssl: {
-    //   require: true,
-    //   rejectUnauthorized: false
-    // },
   migrations: {
     directory: path.join(__dirname, 'src', 'db', 'migrations')
   },
