a6607f7fbead104d224afbd8af8bc90e5ec8cccc
Adnan Ahmed <adnan.ahmed01@gmail.com>
2021-10-04 23:12:34 +0100

    added npm file autoprefixer

--- components/Nav.js
+++ components/Nav.js
@@ -35,8 +35,8 @@ const Nav = (props, {fixed }) => {
                 <NavItem text="HOME" href="/" font="fas fa-home text-lg leading-lg text-white opacity-75"/>
                 <NavItem text="ABOUT" href="/about" font="fas fa-info text-lg leading-lg text-white opacity-75"/>
                 { props.loggedIn && <NavItem text="PRODUCT" href="/product " font="fab fa-product-hunt text-lg leading-lg text-white opacity-75"/> }
-                <NavItem text="STORE" href="/store " font="fas fa-store text-lg leading-lg text-white opacity-75"/>
-                <NavItem text="CART" href="/cart " font="fas fa-shopping-cart text-lg leading-lg text-white opacity-75"/>
+                <NavItem text="STORE" href="/store" font="fas fa-store text-lg leading-lg text-white opacity-75"/>
+                <NavItem text="CART" href="/cart" font="fas fa-shopping-cart text-lg leading-lg text-white opacity-75"/>
                 { !props.loggedIn && <NavItem text="LOGIN" href="/login" font="fa fa-id-badge leading-lg text-white opacity-75"/> } 
                 { !props.loggedIn && <NavItem text="SIGN UP" href="/signup" font="fa fa-id-badge leading-lg text-white opacity-75"/> }
                 { props.loggedIn && <NavItem text="SIGN OUT" href="/" onClick={() => logout()} font="fas fa-door-open text-lg leading-lg text-white opacity-75"/> }
